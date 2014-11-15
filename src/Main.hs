8864c1ff6dd60ba5066e0a703967a791636da81b
Eric Conlon <econlon@twitter.com>
2014-11-15 12:50:17 -0800

    scotty example

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-main = return ()
+import Web.Scotty
+import Data.Monoid (mconcat)
+
+main = scotty 3000 $ do
+  get "/:word" $ do
+    beam <- param "word"
+    html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
