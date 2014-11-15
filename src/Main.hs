a1510cd73a98ba89705376202c8758e851adc5c3
Eric Conlon <econlon@twitter.com>
2014-11-15 13:24:18 -0800

    blazing

--- src/Main.hs
+++ src/Main.hs
@@ -2,10 +2,22 @@
 
 module Main where
 
-import Web.Scotty
 import Data.Monoid (mconcat)
+import Network.Wai.Middleware.RequestLogger
+import qualified Web.Scotty as S
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
+import Text.Blaze.Html.Renderer.Text (renderHtml)
 
-main = scotty 3000 $ do
-  get "/:word" $ do
-    beam <- param "word"
-    html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+blaze :: H.Html -> S.ActionM ()
+blaze = S.html . renderHtml
+
+main :: IO ()
+main = S.scotty 3000 $ do
+  S.middleware logStdoutDev
+
+  S.get "/:word" $ do
+    beam <- S.param "word" :: S.ActionM String
+    blaze $ do
+      H.h1 $ H.toHtml $ "Scotty, " ++ beam ++ " me up!"
+      H.p "yup"
