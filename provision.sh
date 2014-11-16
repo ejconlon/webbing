9865a3b05a1db0b9ce683c26f0bef1e9e26c0613
Eric Conlon <econlon@twitter.com>
2014-11-15 19:48:10 -0800

    fix versions

--- provision.sh
+++ provision.sh
@@ -4,6 +4,10 @@ set -eux
 # Deploys the application on heroku. It is not necessary to build beforehand,
 # but it's probably smart to do so just to confirm everything should work.
 
+# Set appname
+
+APPNAME="${1-}"
+
 # Ensure Heroku is installed
 
 INSTALL_HEROKU="wget -qO- https://toolbelt.heroku.com/install.sh | sh"
@@ -12,23 +16,23 @@ command -v heroku >/dev/null 2>&1 || { $INSTALL_HEROKU; }
 
 # Login
 
-heroku login
+#heroku login
 
 # Provision instance
 
-heroku create --stack=cedar --buildpack \
-  https://github.com/begriffs/heroku-buildpack-haskell.git
+#heroku create --stack=cedar --buildpack \
+#  https://github.com/begriffs/heroku-buildpack-haskell.git
 
 # Add database
 
-heroku addons:add heroku-postgresql:dev
-DBNAME=$(heroku config | grep POSTGRES | cut -d: -f1)
-heroku pg:promote $DBNAME
+#heroku addons:add heroku-postgresql:dev
+#DBNAME=$(heroku config | grep POSTGRES | cut -d: -f1)
+#heroku pg:promote $DBNAME
 
 # Rename
 
-if [ -e "$1" ]; then
-    heroku apps:rename newname
+if [ ! -z "$APPNAME" ]; then
+    heroku apps:rename $APPNAME
 fi
 
 # Deploy
